5082551841f4328d39f56930925f141e0aa081da
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2023-12-23 16:42:02 +0530

    Populating recursions handled.
    Signed-off-by: Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>

--- CryptoApp/app/src/main/java/com/vinodpatildev/cryptoapp/viewmodels/HomeViewModel.kt
+++ CryptoApp/app/src/main/java/com/vinodpatildev/cryptoapp/viewmodels/HomeViewModel.kt
@@ -3,13 +3,13 @@ package com.vinodpatildev.cryptoapp.viewmodels
 import android.app.Application
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.vinodpatildev.cryptoapp.Util.Constant
 import com.vinodpatildev.cryptoapp.Util.Resource
 import com.vinodpatildev.cryptoapp.Util.getCurrentTime
+import com.vinodpatildev.cryptoapp.models.CryptoApiResponseField
 import com.vinodpatildev.cryptoapp.models.CryptoCurrency
 import com.vinodpatildev.cryptoapp.repository.Repository
 import kotlinx.coroutines.Dispatchers
@@ -26,46 +26,37 @@ class HomeViewModel(
     val lastUpdatedTime: MutableLiveData<String> = MutableLiveData("")
 
     init {
-        reloadCryptoCurrencyList()
+        refreshCryptoCurrencyList()
+    }
+
+    private fun refreshCryptoCurrencyList() {
+        viewModelScope.launch(Dispatchers.IO) {
+            cryptoCurrencyList.postValue(Resource.Loading())
+            try {
+                val currencyList = repository.getCurrencyList().data
+                val exchangeRates = repository.getLiveExchangeRates().data
+                val combinedList: List<CryptoCurrency> = combineLists(currencyList, exchangeRates)
+                cryptoCurrencyList.postValue(Resource.Success(combinedList))
+                updateLastUpdateTime()
+                // refresh after 3 minutes
+                handler.postDelayed({ refreshCryptoCurrencyList() }, Constant.NEXT_REFRESTH_INTERVAL)
+            } catch (e: Exception) {
+                cryptoCurrencyList.postValue(Resource.Error(e.message.toString()))
+                if (e is TimeoutException) refreshCryptoCurrencyList()
+            }
+        }
     }
 
     fun reloadCryptoCurrencyList() {
         viewModelScope.launch(Dispatchers.IO) {
             cryptoCurrencyList.postValue(Resource.Loading())
             try {
-                val CurrencyList = repository.getCurrencyList().data
-                Log.d(
-                    "logtesttestt",
-                    "reloadCryptoCurrencyList: CurrencyList" + CurrencyList.toString()
-                )
+                val currencyList = repository.getCurrencyList().data
                 val exchangeRates = repository.getLiveExchangeRates().data
-                Log.d(
-                    "logtesttestt",
-                    "reloadCryptoCurrencyList: exchangeRates" + exchangeRates.toString()
-                )
-
-                val combinedList: List<CryptoCurrency> = CurrencyList.orEmpty()
-                    .entries
-                    .mapNotNull { entry ->
-                        val currencyCode = entry.key
-                        val cryptoCurrencyListApiData = entry.value
-                        val exchangeRate = exchangeRates?.get(currencyCode)
-                        exchangeRate?.let {
-                            CryptoCurrency(
-                                icon_url = cryptoCurrencyListApiData.iconUrl,
-                                exchange_rate = it,
-                                name = cryptoCurrencyListApiData.name,
-                                name_full = cryptoCurrencyListApiData.fullName,
-                                symbol = cryptoCurrencyListApiData.symbol
-                            )
-                        }
-                    }
+                val combinedList: List<CryptoCurrency> = combineLists(currencyList, exchangeRates)
                 cryptoCurrencyList.postValue(Resource.Success(combinedList))
                 updateLastUpdateTime()
-                // refresh after 3 minutes
-                handler.postDelayed({ reloadCryptoCurrencyList() }, Constant.NEXT_REFRESTH_INTERVAL)
             } catch (e: Exception) {
-                Log.d("logtesttest", "exception : " + e.message.toString())
                 cryptoCurrencyList.postValue(Resource.Error(e.message.toString()))
                 if (e is TimeoutException) reloadCryptoCurrencyList()
             }
@@ -74,5 +65,24 @@ class HomeViewModel(
 
     private fun updateLastUpdateTime() {
         lastUpdatedTime.postValue(getCurrentTime())
+    }
+
+    private fun combineLists(CurrencyList :  Map<String, CryptoApiResponseField>?, exchangeRates :  Map<String, Double>?) : List<CryptoCurrency>{
+        return CurrencyList.orEmpty()
+            .entries
+            .mapNotNull { entry ->
+                val currencyCode = entry.key
+                val cryptoCurrencyListApiData = entry.value
+                val exchangeRate = exchangeRates?.get(currencyCode)
+                exchangeRate?.let {
+                    CryptoCurrency(
+                        icon_url = cryptoCurrencyListApiData.iconUrl,
+                        exchange_rate = it,
+                        name = cryptoCurrencyListApiData.name,
+                        name_full = cryptoCurrencyListApiData.fullName,
+                        symbol = cryptoCurrencyListApiData.symbol
+                    )
+                }
+            }
     }
 }
--- CryptoApp/app/src/main/java/com/vinodpatildev/cryptoapp/views/MainActivity.kt
+++ CryptoApp/app/src/main/java/com/vinodpatildev/cryptoapp/views/MainActivity.kt
@@ -2,7 +2,6 @@ package com.vinodpatildev.cryptoapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
